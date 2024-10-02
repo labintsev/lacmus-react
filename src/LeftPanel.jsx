5bc4bf826b9c87d6efba95d3b2afd8b78d9ceb6c
labintsev <ailabintsev@fa.ru>
2024-10-02 22:28:12 +0300

    fix image list

--- src/LeftPanel.jsx
+++ src/LeftPanel.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-function Button(){
+function OpenButton(){
     return (
         <div className="btn-container">
             <label className="pure-button center" htmlFor="image_uploads">Open</label>
@@ -15,32 +15,43 @@ function Button(){
 }
 
 
+function PredictButton(){
+  return (
+    <div className="btn-container">
+      <div className="pure-button center">Predict all</div>
+    </div>
+  )
+}
+
 
 function ImagePreview(props){
-    let src = props.src
     return (
-        <image 
-            src={src}
-            width={16}
-            height={16}
+        <img 
+            src={URL.createObjectURL(props.src)}
+            width={32}
+            height={32}
         />
     )
 }
+
 
 function LeftPanel() {
     const [files, setFiles] = useState([]);
 
     const renderFileList = () => (
-        <ol>
-          {[...files].map((f, i) => (
-            <ImagePreview src={f.resolve} />
+        <ul>
+          {[...files].map(f => (
+            <li key={f.name}>
+              <ImagePreview src={f} />
+            </li>
           ))}
-        </ol>
+        </ul>
       )
 
     return (
       <div className="pure-u-1-5 left-panel center">
-        
+        <PredictButton />
+
         {renderFileList()}
 
         <input type="file" 
