3551e4dbf850314fd91f6ae9e145136e396c72d8
ailabintsev <andrej.labintsev@yandex.ru>
2024-12-13 21:12:36 +0300

    refactor css

--- src/LeftPanel.jsx
+++ src/LeftPanel.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { useRef } from 'react';
 
 function ImagePreview(props) {
 
@@ -17,28 +18,39 @@ function ImagePreview(props) {
 
 function LeftPanel({ setImageFile }) {
   const [imgList, setImgList] = useState([]);
+  const inputFile = useRef(null)
 
   const renderFileList = () => (
     <div className='image-list'>
 
-        {[...imgList].map(imgFile => (
-            <ImagePreview
-              imgFile={imgFile}
-              setImageFile={setImageFile} />
-        ))}
+      {[...imgList].map(imgFile => (
+        <ImagePreview
+          imgFile={imgFile}
+          key={imgFile.name}
+          setImageFile={setImageFile} />
+      ))}
 
     </div>
-
   )
+
+  const onButtonClick = () => {
+    inputFile.current.click();
+  };
 
   return (
     <div className="left-panel">
 
       <input type="file"
+        id='file'
+        ref={inputFile}
+        style={{ display: 'none' }}
         accept="image/*"
         multiple
         onChange={(e) => setImgList(e.target.files)} />
-      
+
+
+        <button onClick={onButtonClick} className='pure-button open-button'>Open files</button>
+
       {renderFileList()}
     </div>
   );
